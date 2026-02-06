e37e4c5a52bf876c3a3dca7564c198ba82e45986
Juan Acuña <juan.acuna1@estudiantesunibague.edu.co>
2026-02-06 09:07:57 -0500

    Notificaciones

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,8 @@ public class Main
 {
     public static void main(String[] args)
     {
-        CuentaBancaria cuenta1 = new CuentaBancaria( id: "1", saldoInicial: 100);
+        CuentaBancaria cuenta1 = new CuentaBancaria( id;Object saldoInicial;
+        "1", saldoInicial: 100);
         cuenta1.retirar( valorARetirar: 100);
         cuenta1.consignar( valorAConsignar: 150);
         System.out.printf("El saldo de la cuenta es: " + cuenta1.getSaldo());
