05f0624a316a7c227a6639cd84e0047581eab057
Juan Acuña <juan.acuna1@estudiantesunibague.edu.co>
2026-02-06 09:21:47 -0500

    Implementacion herencia y polimorfismo

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,15 @@
-public class Main
-{
-    public static void main(String[] args)
-    {
-        CuentaBancaria cuenta1 = new CuentaBancaria( id;Object saldoInicial;
-        "1", saldoInicial: 100);
-        cuenta1.retirar( valorARetirar: 100);
-        cuenta1.consignar( valorAConsignar: 150);
-        System.out.printf("El saldo de la cuenta es: " + cuenta1.getSaldo());
+public class Main {
+    public static void main(String[] args) {
+        CuentaBancaria cuenta1 = new CuentaBancaria(  "1",  100);
+        cuenta1.retirar(  100);
+        cuenta1.consignar (  150);
+        System.out.println("Elsaldo de la cuenta es: " + cuenta1.getSaldo());
+
+        Notificacion[ ] notificacion = new Notificacion [ 2];
+        notificacion[0] = new NotificacionCorreo( "Pepito");
+        notificacion [1] = new NotificacionSMS ( "Juanillo");
+
+        notificacion[0 ].enviarMensaje( "hola mi amigo");
+        notificacion[1].enviarMensaje( "Hola mi broco");
     }
 }
